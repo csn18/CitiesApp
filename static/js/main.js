b6afd89c003ed82dbda346d1f6b8769e953c0db9
csn-dev <fr1m4334@gmail.com>
2019-12-24 18:17:22 +0300

    fixed code in task6.html
    implementation of search through a method is added Ajax
    
    added ajax function for dropdown (main.js and server.py) (did not finish)

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,79 @@
 
+let countryId = '';
+
 function ajaxPagination() {
-    $('#pagination a.page-link').each((index, el) => {
-        $(el).click((e) => {
-            e.preventDefault();
-            let page_url = $(el).attr('href');
-            console.log( page_url );
+  $('#pagination a.page-link').each((index, el) => {
+      $(el).click((e) => {
+          e.preventDefault();
+            let getHrefSplit = $(el).attr('href').split('&')[1]
+            let pageUrl = `?countries_id=${countryId}&${getHrefSplit}`
+            console.log(getHrefSplit)
 
-            $.ajax({
-                url: page_url,
-                type: 'GET',
-                success: (data) => {
-                    $('#city').empty();
-                    $('#city').append( $(data).find('#city') );
+          $.ajax({
+              url: pageUrl,
+              type: 'GET',
+              success: (data) => {
+                  $('#cities').empty();
+                  $('#cities').append( $(data).find('#cities') );
 
 
-                    $('#pagination').empty();
-                    $('#pagination').append( $(data).find('#pagination').html() );
-                }
-            })
-        })
+                  $('#pagination').empty();
+                  $('#pagination').append( $(data).find('#pagination').html() );
+              }
+          })
+      })
+  })
+}
+ 
+
+function ajaxDropdown() {
+  $('#dropdown a.dropdown-item').each((index, el) => {
+      $(el).click((e) => {
+          e.preventDefault();
+          let getHref = $(el).attr('href')
+          console.log(getHref)
+
+          $.ajax({
+            url: '/task6dropdown',
+            type: 'GET',
+            success: (result) => {
+              countryId = result['countryId']
+              country = result['country']
+              console.log(countryId)
+              console.log(country)
+
+              $('#dropdown').empty()
+              for (let i = 0; i < 5; i++) {
+              $('#dropdown').append(`<a class="dropdown-item" href="?countries_id=${countryId}">${country}</a>`)}            }
+          })
+      })
+  })
+} 
+
+function ajaxSearch() {
+  $(document).ready(function(){
+    $('#searchBox').on('input', function(e){
+      textInSearchBox = $('#searchBox').val();
+
+      $.ajax({
+        url: '/task6search',
+        type: 'POST',
+        data: {text: textInSearchBox},
+        success: (result) => {
+          countryId = result['countryId']
+          res = result['cities']
+
+
+          if(result) {
+            $('#listGroup').empty();
+            for (let i = 0; i < 5; i++) {
+            $('#listGroup').append(`<li class="list-group-item">${res[i]}</li>`)
+            }
+          }
+        }
+      })
     })
+  })
 }
 
 
@@ -28,9 +82,18 @@ window.onload = function() {
   if (originalOnload) {
     originalOnload();
   }
+
   $(document).ready(function() {
-      ajaxPagination()
+    ajaxDropdown()
     })
+
+  $(document).ready(function() {
+    ajaxSearch()
+    })
+  
+  $(document).ready(function() {
+    ajaxPagination()
+  })
 
     $(document).ajaxStop(function() {
       ajaxPagination()
