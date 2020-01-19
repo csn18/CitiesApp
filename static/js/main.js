b13ebd8cf7bc81741f28d37370697fb0b3953051
csn18 <formcsp@gmail.com>
2020-01-19 14:15:45 +0300

    [WIP]task7

--- static/js/main.js
+++ static/js/main.js
@@ -1,72 +1,113 @@
-
 let countryId = '';
 
 function ajaxPagination() {
-  $('#pagination a.page-link').each((index, el) => {
-      $(el).click((e) => {
-          e.preventDefault();
-            let getHrefSplit = $(el).attr('href').split('&')[1]
+    $('#pagination a.page-link').each((index, el) => {
+        $(el).click((e) => {
+            e.preventDefault();
+            let getHrefSplit = $(el).attr('href').split('&')[1];
             countryId = countryId ? countryId : $(el).attr('href').split('=')[1][0];
-            let pageUrl = `?countries_id=${countryId}&${getHrefSplit}`
+            let pageUrl = `?countries_id=${countryId}&${getHrefSplit}`;
 
-          $.ajax({
-              url: pageUrl,
-              type: 'GET',
-              success: (data) => {
-                  $('#cities').empty();
-                  $('#cities').append( $(data).find('#cities') );
+            $.ajax({
+                url: pageUrl,
+                type: 'GET',
+                success: (data) => {
+                    $('#cities').empty();
+                    $('#cities').append($(data).find('#cities'));
 
 
-                  $('#pagination').empty();
-                  $('#pagination').append( $(data).find('#pagination').html() );
-              }
-          })
-      })
-  })
+                    $('#pagination').empty();
+                    $('#pagination').append($(data).find('#pagination').html());
+                }
+            })
+        })
+    })
 }
 
 
 function ajaxSearch() {
-  $(document).ready(function(){
-    $('#searchBox').on('input', function(e){
-      textInSearchBox = $('#searchBox').val();
+    $(document).ready(function () {
+        $('#searchBox').on('input', function (e) {
+            textInSearchBox = $('#searchBox').val();
 
-      $.ajax({
-        url: '/search',
-        type: 'GET',
-        data: {text: textInSearchBox},
-        success: (result) => {
-          countryId = result['countryId']
-          cities = result['cities']
+            $.ajax({
+                url: '/search',
+                type: 'GET',
+                data: {text: textInSearchBox},
+                success: (result) => {
+                    countryId = result['countryId'];
+                    cities = result['cities'];
 
 
-          if(result) {
-            $('#listGroup').empty();
-            for (let i = 0; i < 5; i++) {
-            $('#listGroup').append(`<li class="list-group-item">${cities[i]}</li>`)
-            }
-          }
-        }
-      })
+                    if (result) {
+                        $('#listGroup').empty();
+                        for (let i = 0; i < 5; i++) {
+                            $('#listGroup').append(`<li class="list-group-item">${cities[i]}</li>`)
+                        }
+                    }
+                }
+            })
+        })
     })
-  })
+}
+
+
+function initMap() {
+    let opt = {
+        center: {lat: 0, lng: 0},
+        zoom: 2
+    };
+
+    geocode();
+
+    function geocode() {
+        let myMap = new google.maps.Map(document.getElementById('map'), opt);
+        let apiKey = 'AIzaSyAJSa41jnaiHI4OD54Vg507O0qQKcXeY-0';
+        let city = ['Москва', 'Казань', 'Севастополь', 'Екатеринбург', 'Киров'];
+        for (let nameCity of city) {
+            axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: nameCity,
+                    key: apiKey
+                }
+            }).then(function (response) {
+
+
+                let lat = response.data.results[0].geometry.location.lat;
+                let lng = response.data.results[0].geometry.location.lng;
+
+
+                let marker = new google.maps.Marker({
+                    position: {lat: lat, lng: lng},
+                    map: myMap,
+                    title: nameCity
+
+                });
+
+
+            }).catch(function (error) {
+                console.log(error);
+            })
+        }
+    }
 }
 
 
 originalOnload = window.onload;
-window.onload = function() {
-  if (originalOnload) {
-    originalOnload();
-  }
-    $(document).ready(function() {
-    ajaxPagination()
+window.onload = function () {
+    if (originalOnload) {
+        originalOnload();
+    }
+    $(document).ready(function () {
+        ajaxPagination()
     })
 
-    $(document).ajaxStop(function() {
-      ajaxPagination()
+    $(document).ajaxStop(function () {
+        ajaxPagination()
     })
 
-    $(document).ready(function(){
-    ajaxSearch()
+    $(document).ready(function () {
+        ajaxSearch()
     })
+
 };
